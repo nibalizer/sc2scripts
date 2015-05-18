8fb0003f0e4dad7de3f6fa8fc9b9d18d8ed4913d
Spencer Krum <nibz@cat.pdx.edu>
2015-05-17 21:07:34 -0700

    Do summary processing

--- zealot_counter.py
+++ zealot_counter.py
@@ -28,7 +28,7 @@ def init_analyzer():
     return paths
 
 
-def analyze_replay(replay):
+def analyze_replay(replay, result_data):
 
     # check if a protoss is playing
     races = [i.play_race for i in replay.players]
@@ -47,19 +47,32 @@ def analyze_replay(replay):
         # bail if this player is not Protoss
         if player.play_race != 'Protoss':
             continue
+        print(player.name)
 
         zs = [i for i in player.units if i.name == 'Zealot']
         zealots_made = len(zs)
         dead_zs = [i for i in zs if i.died_at is not None]
         zealots_died = len(dead_zs)
 
-
         print ("Zealots made: {0}".format(zealots_made))
         print ("Zealots died: {0}".format(zealots_died))
         print ("Zealots that made it: {0}".format(zealots_made - zealots_died))
+        if (zealots_made - zealots_died) > 0:
+            result_data["zealots_lived"] += 1
+            result_data["total_zealots_lived"] += (zealots_made - zealots_died)
+        else:
+            result_data["zealots_all_died"] += 1
 
 
 if __name__ == '__main__':
+    result_data = {}
+    result_data["zealots_lived"] = 0
+    result_data["zealots_all_died"] = 0
+    result_data["total_zealots_lived"] = 0
     paths = init_analyzer()
     for replay in sc2reader.load_replays(paths, debug=True):
-        analyze_replay(replay)
+        analyze_replay(replay, result_data)
+    print ("###")
+    print ("Number of games where zealots all died: ", result_data["zealots_all_died"])
+    print ("Number of games where some zealots lived: {0}".format(result_data["zealots_lived"]))
+    print ("Total number of zealots that lived: {0}".format(result_data["total_zealots_lived"]))
